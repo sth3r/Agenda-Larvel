3ed91ab4dc9e2bfe592f2b67dfbe80143c893cea
sth3r <vrodrigues.esther@gmail.com>
2024-12-13 20:42:49 -0300

    balde em forma de paste, rotas do rest oalem de tarefa nao funciona

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class TarefaController extends Controller
 {
     public function index(){
-        return view('tarefas', ['tarefas' => Tarefa::all()]);
+        // return view('tarefas', ['tarefas' => Tarefa::all()]);
+        return view('tarefa.index', ['tarefas'=>Tarefa::all()]);
         // return response()->json(Tarefa::all());
     }
 
--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -1,6 +1,11 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tarefas</title>
+</head>
 <body>
     <h1>Tarefas</h1>
     @if ($tarefas->count()>0)
