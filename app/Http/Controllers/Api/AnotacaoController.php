426ad7af28b5d57050281ccfbe4cbfc6462301f7
sth3r <vrodrigues.esther@gmail.com>
2025-01-17 08:55:17 -0300

    coiso dos api criado

---
+++ app/Http/Controllers/Api/AnotacaoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Anotacao;
+use Illuminate\Http\Request;
+
+class AnotacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Anotacao $anotacao)
+    {
+        //
+    }
+}
